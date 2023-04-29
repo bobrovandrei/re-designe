8b2bdbc783ee0e9775754d51239a0c70f1a97c78
bobrovandrei <bobrow_@tut.by>
2023-04-29 15:42:15 +0200

    fix

--- page1/page1.js
+++ page1/page1.js
@@ -1,9 +1,9 @@
 document.body.addEventListener("click", (e) => {
-  if (!e.target.matches("button")) return;
+  if (!e.target.matches("#program")) return;
   document.querySelector(".pic img" ).src = e.target.dataset.src;
 
   document
-    .querySelectorAll("button")
+    .querySelectorAll("#program")
     .forEach((btn) => btn.classList.remove("active"));
   e.target.classList.add("active");
 });
